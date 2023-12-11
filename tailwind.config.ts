7e57fbfc294869654118630e146492fdd13cb938
boranfurkan <furkanboran123@gmail.com>
2023-12-11 16:16:29 +0000

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,19 +2,25 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      backgroundImage: {},
+    },
+    fontSize: {
+      "fs-large-bold": "1.3125rem", // 21px
+      "fs-medium-normal": "1.125rem", // 18px
+      "fs-huge-extrabold": "8.125rem", // 130px
+      "fs-larger-medium": "1.375rem", // 22px
+      "fs-xlarge-extrabold": "4.6875rem", // 75px
+      "fs-xl-bold": "1.75rem", // 28px
+      "fs-large-medium": "1.25rem", // 20px
+      "fs-larger-bold": "1.5625rem", // 25px
     },
   },
   plugins: [],
-}
+};
 export default config
