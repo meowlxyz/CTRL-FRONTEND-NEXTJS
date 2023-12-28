1283cae5f5aa0316c57ff3b4a19b3a0a03aa260d
0xpeche <114630968+0xpeche@users.noreply.github.com>
2023-12-28 13:09:15 +0100

    fix: balls

--- components/shared/ellipse/Ellipse.tsx
+++ components/shared/ellipse/Ellipse.tsx
@@ -1,12 +1,32 @@
+"use client";
 import React from "react";
 
 const Ellipse = ({ width, classes }: { width: number; classes?: string }) => {
-  return (
-    <div
+  const [isDesktop, setIsDesktop] = React.useState<boolean>(false);
+
+  const handleResize = () => {
+    setIsDesktop(window.innerWidth > 768);
+  };
+
+  React.useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  if (isDesktop) {
+    return (
+      <div
+        className={`ellipse-shadow aspect-square rounded-full ${classes}`}
+        style={{ width: `${width}px` }}
+      ></div>
+    );
+  } else {
+    return (<div
       className={`ellipse-shadow aspect-square rounded-full ${classes}`}
-      style={{ width: `${width}px` }}
-    ></div>
-  );
+      style={{ width: `${width}px`, visibility: `hidden` }}
+    ></div>)
+  }
+
 };
 
 export default Ellipse;
